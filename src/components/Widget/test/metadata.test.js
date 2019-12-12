30083391b894a4fdcd47af06bedbf25e5bfc4d1a
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-12-12 17:01:39 -0500

    refactor: add store test for userInput, linkTarget, messageTarget

---
+++ src/components/Widget/test/metadata.test.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+
+import assetMock from 'tests-mocks/fileMock';
+import Widget from '../index';
+import { store, initStore } from '../../../store/store';
+
+describe('Metadata', () => {
+  const profile = assetMock;
+  const handleUserMessage = jest.fn();
+
+
+  class LocalStorageMock {
+    constructor() {
+      this.store = {};
+    }
+
+    clear() {
+      this.store = {};
+    }
+
+    getItem(key) {
+      return this.store[key] || null;
+    }
+
+    setItem(key, value) {
+      this.store[key] = value.toString();
+    }
+
+    removeItem(key) {
+      delete this.store[key];
+    }
+  }
+
+  const localStorage = new LocalStorageMock();
+
+
+  initStore('dummy', 'dummy', 'dummy', localStorage);
+  store.dispatch({
+    type: 'CONNECT' });
+  const widgetComponent = shallow(
+    <Provider store={store}>
+      <Widget
+        store={store}
+        handleNewUserMessage={handleUserMessage}
+        profileAvatar={profile}
+        dispatch={store.dispatch}
+        connectOn="open"
+        customMessageDelay={() => {}}
+        connected
+        isChatOpen={false}
+      />
+    </Provider>
+
+  );
+
+  it('userinput metaData should change input info in store', () => {
+    let botUtter = {
+      metadata: {
+        userInput: 'disable'
+      }
+    };
+    widgetComponent.dive().dive().instance().handleBotUtterance(botUtter);
+    expect(store.getState().metadata.get('userInput')).toEqual('disable');
+    botUtter = {
+      metadata: {
+        userInput: 'hide'
+      }
+    };
+    widgetComponent.dive().dive().instance().handleBotUtterance(botUtter);
+    expect(store.getState().metadata.get('userInput')).toEqual('hide');
+  });
+
+  it('linktarget should change link targets info in store', () => {
+    let botUtter = {
+      metadata: {
+        linkTarget: '_self'
+      }
+    };
+    widgetComponent.dive().dive().instance().handleBotUtterance(botUtter);
+    expect(store.getState().metadata.get('linkTarget')).toEqual('_self');
+
+    botUtter = {
+      metadata: {
+        linkTarget: '_blank'
+      }
+    };
+    widgetComponent.dive().dive().instance().handleBotUtterance(botUtter);
+    expect(store.getState().metadata.get('linkTarget')).toEqual('_blank');
+  });
+
+
+  it('messageTarget should change targets info in store', () => {
+    let botUtter = {
+      text: 'dummy',
+      metadata: {
+        messageTarget: 'tooltip_init'
+      }
+    };
+    widgetComponent.dive().dive().instance().handleBotUtterance(botUtter);
+
+    expect(store.getState().metadata.get('tooltipMessage')).toEqual('dummy');
+    expect(store.getState().metadata.get('tooltipDisplayed')).toEqual(true);
+
+    botUtter = {
+      text: 'dummy',
+      metadata: {
+        messageTarget: 'tooltip_always'
+      }
+    };
+    widgetComponent.dive().dive().instance().handleBotUtterance(botUtter);
+    expect(store.getState().metadata.get('tooltipMessage')).toEqual('dummy');
+    expect(store.getState().metadata.get('tooltipDisplayed')).toEqual(true);
+  });
+});
+
