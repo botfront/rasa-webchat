7df96776a967691f249ce22d13bc98812a10e38f
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-12-19 10:00:03 -0500

    refactor: removes unused action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -226,13 +226,6 @@ export function setUserInput(userInputState) {
   };
 }
 
-export function messageTarget(target) {
-  return {
-    type: actions.SET_MESSAGE_TARGET,
-    target
-  };
-}
-
 export function setPageChangeCallbacks(pageChangeCallbacks) {
   return {
     type: actions.SET_PAGECHANGE_CALLBACKS,
