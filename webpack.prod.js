1b1acee96f77b966596ff044597b349f5bc5874a
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-01-16 10:26:46 -0500

    refactor: create two differents builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,11 @@
-'use strict'
+
 
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
+module.exports = [{
   // entry: ['babel-polyfill', './index.js'],
   entry: './index.js',
-  externals: {
-    react: 'react',
-    reactDOM: 'react-dom'
-  },
   output: {
     path: path.join(__dirname, '/lib'),
     filename: 'index.js',
@@ -49,4 +45,50 @@ module.exports = {
     ]
   },
   plugins: [new CleanWebpackPlugin(['lib'])]
-};
+}, {
+  entry: './index.js',
+  externals: {
+    react: 'react',
+    reactDOM: 'react-dom'
+  },
+  output: {
+    path: path.join(__dirname, '/es'),
+    filename: 'index.js',
+    library: 'WebChat',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [path.resolve(__dirname, 'src/scss/')]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: 'url-loader'
+        }
+      }
+    ]
+  },
+  plugins: [new CleanWebpackPlugin(['es'])]
+}
+];
