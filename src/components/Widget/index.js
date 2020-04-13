47daa287fdb2d6028f12cf6f3c1cb52847e59815
Matthieu Joannon <mattjoannon@gmail.com>
2020-04-13 16:58:33 -0400

    feat: automatically scroll to highlights

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -300,6 +300,24 @@ class Widget extends Component {
             }
         }
       });
+      // We check that the method is here to prevent crashes on unsupported browsers.
+      if (elements[0] && elements[0].scrollIntoView) {
+        if (/Mobi/.test(navigator.userAgent)) {
+          elements[0].scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
+        } else {
+          const rectangle = elements[0].getBoundingClientRect();
+
+          const ElemIsInViewPort = (
+            rectangle.top >= 0 &&
+              rectangle.left >= 0 &&
+              rectangle.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+              rectangle.right <= (window.innerWidth || document.documentElement.clientWidth)
+          );
+          if (!ElemIsInViewPort) {
+            elements[0].scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
+          }
+        }
+      }
     }
   }
 
