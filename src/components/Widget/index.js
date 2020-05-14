e58278495ace6bc0dbffa1941a6d960878828e0d
Matthieu Joannon <mattjoannon@gmail.com>
2020-05-14 13:52:57 -0400

    chore: typo

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -126,6 +126,9 @@ class Widget extends Component {
           dispatch(addUserMessage(text, tooltipSelector));
         } else if (tooltipSelector) {
           dispatch(addUserMessage('hidden', tooltipSelector, true));
+        }
+        if (tooltipSelector) {
+          dispatch(closeChat());
           showTooltip(true);
         }
       };
