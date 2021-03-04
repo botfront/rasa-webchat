de2d1b3f57871d8886947b256b1f5ec6945fa3f5
mfenej <mfenej@dtp.ae>
2021-03-04 15:41:18 +0200

    try polling transport for socket.io

--- src/socket-socketio.js
+++ src/socket-socketio.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 export default function (socketUrl, customData, path) {
   // const options = path ? { path } : {};
-  const socket = io(socketUrl, { transports: ['websocket'] });
+  const socket = io(socketUrl, { transports: ['polling'] });
   socket.on('connect', () => {
     console.log(`connect:${socket.id}`);
     socket.customData = customData;
