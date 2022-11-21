08e1c9dcc7e7cb94fb9b3bdcb21b9672b8e1928b
lishanda <sasha901239@gmail.com>
2022-11-21 09:37:25 +0300

    Fixed storage clear

--- index.js
+++ index.js
@@ -208,7 +208,16 @@ export default React.forwardRef((props, ref) => (
 
 export const selfMount = (props, element = null) => {
   const load = () => {
-    window.localStorage.clear()
+
+    for (let i=0; i<window.localStorage.length; i++) {
+      let key = window.localStorage.key(i);
+      if (key.includes("cookiename_")) {
+      window.localStorage.removeItem(key)
+      }
+    }
+    window.localStorage.removeItem("chat_session")
+    window.localStorage.removeItem("rasaWebchatPro")
+
     if (element === null) {
       const node = document.createElement('div');
       node.setAttribute('id', 'rasaWebchatPro');
